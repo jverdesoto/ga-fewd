e31a37ccd7f859a62021e5dafee717fdeeac570f
Javi Verdesoto <verdesoto.diseno@gmail.com>
2023-08-09 14:58:34 -0500

    Responsive CSS and Intro to JS

---
+++ week-03/js/scripts.js
@@ -0,0 +1,26 @@
+var numberOne = '1';
+
+var numberTwo = 1
+
+var bool = false
+
+// if ( numberOne === numberTwo ) {
+//     console.log("YES THEY ARE THE SAME");
+// }
+
+// if ( numberOne === numberTwo ) {
+//     console.log("YES THEY ARE THE SAME");
+// } else {
+//     console.log("NO THEY ARE DIFFERENT");
+// }
+
+if ( numberOne === numberTwo ) {
+    console.log("YES THEY ARE THE SAME");
+} else if ( numberOne == numberTwo ) {
+    console.log("YES THEY ARE THE SAME BUT DIFFERNT TYPES");
+} else {
+    console.log("NO THEY ARE DIFFERENT");
+}
+
+
+console.log(bool);
