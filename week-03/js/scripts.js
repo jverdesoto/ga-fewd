bf77cee9419c8c6d5808794d57326d9f7b4cb898
Areej Almarri <areej@Areejs-MacBook-Pro-3.local>
2023-08-09 22:58:42 +0300

    Responsive css and intro to js

---
+++ week-03/js/scripts.js
@@ -0,0 +1,25 @@
+var numberOne = '1';
+
+var numberTwo = 1
+
+var bool = false
+
+// if ( numberOne === numberTwo) {
+//     console.log("YES THEY ARE THE SAME");
+// }
+
+// if ( numberOne === numberTwo) {
+//     console.log("YES THEY ARE THE SAME");
+// } else {
+//     console.log("NO THEY ARE DIFFERENT");
+// }
+
+if ( numberOne === numberTwo) {
+    console.log("YES THEY ARE THE SAME");
+} else if ( numberOne === numberTwo) {
+    console.log("YES THEY ARE THE SAME BUT DIFFERENT TYPES");
+} else {
+    console.log("NO THEY ARE DIFFERENT");
+}
+
+console. log(bool);
