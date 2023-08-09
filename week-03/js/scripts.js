db1edeb4ae4fa943528acdfabb28cd7e5de46349
Henry Pearce <hal_pearce@hotmail.co.uk>
2023-08-09 20:58:41 +0100

    Responsiven CSS and Intro to JS

---
+++ week-03/js/scripts.js
@@ -0,0 +1,27 @@
+var numberOne = '1';
+
+var numberTwo = 1;
+
+var bool = false;
+
+// if ( numberOne === numberTwo ) {
+//     console.log('Yes they are the same')
+// }
+
+// if (numberOne === numberTwo) {
+//     console.log('Yes they are the same')
+// } else {
+//     console.log('No they are different')
+// }
+
+if (numberOne === numberTwo) {
+    console.log('Yes they are the same')
+} else if (numberOne == numberTwo) {
+    console.log('Yes they are the same but different types')
+} else {
+    console.log('No they are different')
+}
+
+
+
+console.log(bool);
