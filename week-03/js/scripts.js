b179f3c7decf5526cc1a5ff71120a5d032e3bc8e
Freddie McNicholas <freddiemcnicholas@mp-am-fvfgq8uzq05n.lan>
2023-08-09 20:58:38 +0100

    Responsiveness and Intro to JS

---
+++ week-03/js/scripts.js
@@ -0,0 +1,20 @@
+var numberOne = 1
+
+var numberTwo = 1
+
+var bool = false
+
+console.log(bool);
+
+if ( numberOne === numberTwo) {
+    console.log("YES THEY ARE THE SAME")
+} else if (numberOne == numberTwo) {
+    console.log("YES THEY ARE THE SAME BUT DIFFERENT TYPES")
+} else {
+    console.log("DIFFERENT")
+}
+
+console.log(bool);
+
+
+
