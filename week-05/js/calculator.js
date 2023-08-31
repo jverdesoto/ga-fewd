0aa8abaa7d172cd40b7d05fe975578545a064453
Henry Pearce <hal_pearce@hotmail.co.uk>
2023-08-31 19:59:53 +0100

    week5 homework

--- week-05/js/calculator.js
+++ week-05/js/calculator.js
@@ -22,37 +22,70 @@ function addTwoNumbers (numOne, numTwo) {
     return numOne + numTwo
 }
 
-document.getElementById('mySubmit').addEventListener('click', (e) => {
-    e.preventDefault
+document.getElementById('addSubmit').addEventListener('click', (e) => {
+    e.preventDefault();
     document.getElementById('addResult').innerHTML = addTwoNumbers(parseInt(document.getElementById('numOne').value), parseInt(document.getElementById('numTwo').value))
 })
 
-function subtractTwoNumbers (numOne, numTwo) {
-    return numOne =- numTwo
+function subtractTwoNumbers (numThree, numFour) {
+    return numThree - numFour
 }
 
-document.getElementById('mySubmit').addEventListener('click', (e) => {
-    e.preventDefault
-    document.getElementById('result').innerHTML = subtractTwoNumbers(parseInt(document.getElementById('numOne').value), parseInt(document.getElementById('numTwo').value))
+document.getElementById('subtractSubmit').addEventListener('click', (e) => {
+    e.preventDefault();
+    document.getElementById('subtractResult').innerHTML = subtractTwoNumbers(parseInt(document.getElementById('numThree').value), parseInt(document.getElementById('numFour').value))
 })
 
-function multiplyTwoNumbers (numOne, numTwo) {
-    return numOne * numTwo
+function multiplyTwoNumbers (numFive, numSix) {
+    return numFive * numSix
 }
 
-document.getElementById('mySubmit').addEventListener('click', (e) => {
-    e.preventDefault
-    document.getElementById('result').innerHTML = multiplyTwoNumbers(parseInt(document.getElementById('numOne').value), parseInt(document.getElementById('numTwo').value))
+document.getElementById('multiplySubmit').addEventListener('click', (e) => {
+    e.preventDefault();
+    document.getElementById('multiplyResult').innerHTML = multiplyTwoNumbers(parseInt(document.getElementById('numFive').value), parseInt(document.getElementById('numSix').value))
 })
 
-function divideTwoNumbers (numOne, numTwo) {
-    return numOne + numTwo
+function divideTwoNumbers (numSeven, numEight) {
+    if (numEight !== 0) {
+        return numSeven / numEight
+    } else {
+        return "Cannot divide by zero";
+    }
+    
 }
 
-document.getElementById('mySubmit').addEventListener('click', (e) => {
-    e.preventDefault
-    document.getElementById('result').innerHTML = divideTwoNumbers(parseInt(document.getElementById('numOne').value), parseInt(document.getElementById('numTwo').value))
+document.getElementById('divideSubmit').addEventListener('click', (e) => {
+    e.preventDefault();
+    document.getElementById('divideResult').innerHTML = divideTwoNumbers(parseInt(document.getElementById('numSeven').value), parseInt(document.getElementById('numEight').value))
 })
+
+document.getElementById('calculateSubmit').addEventListener('click', (e) => {
+    e.preventDefault();
+
+    const numNine = parseInt(document.getElementById('numNine').value);
+    const numTen = parseInt(document.getElementById('numTen').value);
+    const operator = document.getElementById('operator').value;
+
+    let result;
+
+    if (operator === 'Add') {
+        result = numNine + numTen;
+    } else if (operator === 'Subtract') {
+        result = numNine - numTen;
+    } else if (operator === 'Multiply') {
+        result = numNine * numTen;
+    } else if (operator === 'Divide') {
+        if (numTen !== 0) {
+            result = numNine / numTen
+        } else {
+            result = "Cannot divide by zero";
+        }
+    }
+
+    document.getElementById('calculateResult').innerHTML = result;
+});
+    
+
 
 console.log(addTwoNumbers(2,5));
 
