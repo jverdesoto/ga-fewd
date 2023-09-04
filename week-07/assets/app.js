cfa20bef4df013d4252313b3928461c17830c05d
Javi Verdesoto <verdesoto.diseno@gmail.com>
2023-09-04 14:48:17 -0500

    Sass with JS functionalities and Create-react-app

---
+++ week-07/assets/app.js
@@ -0,0 +1,9 @@
+document.getElementById('theme').addEventListener('click', function () {
+    let conatiner = document.getElementById('container')
+    
+    if (container.classList.contains('dark')) {
+        container.classList.remove('dark')    
+    } else {
+        container.classList.add('dark')
+    }
+})
