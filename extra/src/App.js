f42401d001356238c66b1a7a97370236407f9ab6
Henry Pearce <hal_pearce@hotmail.co.uk>
2023-09-11 21:03:11 +0100

    working with react

--- extra/src/App.js
+++ extra/src/App.js
@@ -1,12 +1,37 @@
-import './App.css';
-import Header from './components/Header';
+import {Container, Stack, Button} from 'react-bootstrap';
+import BudgetCard from './components/BudgetCard';
+import AddBudgetsModal from './components/AddBudgetsModal'; 
+import { useState } from 'react';
+
 
 function App() {
+  const [showAddBudgetModal, setShowAddBudgetModal] = useState(false)
   return (
-    <div className="App">
-      <Header />
-    </div>
-  );
+    <>
+  <Container>
+    <Stack direction='horizontal' gap={2} className='mb-4'>
+      <h1 className='me-auto'>Budget</h1>
+      <Button variant='primary' onClick={()=>{setShowAddBudgetModal(true)}}>Add Budget</Button>
+      <Button variant='secondary'>Add Expense</Button>
+    </Stack>
+    <div 
+      style={{
+        display: 'grid', 
+        gridTemplateColumns: 'repeat(auto-fill, minmax(300px, 1fr))',
+        gap: '1rem', 
+        alignItems: 'flex-start'
+      }}
+      >
+       <BudgetCard name='Areej' amount={1001} maxAmount={1000} />
+       <BudgetCard name='Henry' amount={500} maxAmount={1000}/>
+       <BudgetCard name='Javi'  amount={750} maxAmount={1000}/>
+       <BudgetCard name='Uncategorised'  amount='' maxAmount='' gray />
+       <AddBudgetsModal show={showAddBudgetModal} onClick={() => {setShowAddBudgetModal(false)}} />
+      </div>
+  </Container>
+  <AddBudgetsModal show={showAddBudgetModal} handleClose={() => setShowAddBudgetModal(false)}/>
+  </>
+  )
 }
 
 export default App;
--- extra/src/index.js
+++ extra/src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
 import App from './App';
+import { BudgetsProvider } from './context/BudgetProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BudgetsProvider>
+      <App />
+    </BudgetsProvider>    
   </React.StrictMode>
 );
 
