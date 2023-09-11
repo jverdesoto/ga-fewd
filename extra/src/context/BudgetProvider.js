f44fe7e5c5a74f5731494977166a1f987fdecc98
Freddie McNicholas <freddiemcnicholas@mp-am-fvfgq8uzq05n.lan>
2023-09-11 21:03:08 +0100

    Working with react

---
+++ extra/src/context/BudgetProvider.js
@@ -0,0 +1,59 @@
+import { createContext, useContext, useState } from "react"
+import { v4 as uuidv4} from 'uuid'
+import useLocalStorage from "../hooks/useLocalStorage"
+
+const BudgetContext = createContext()
+
+export function useBudgets () {
+    return useContext(BudgetContext)
+}
+
+export const BudgetsProvider = ({children}) => {
+    const [budgets, setBudgets] = useLocalStorage('budgets', [])
+    const [expenses, setExpenses] = useLocalStorage('expenses', [])
+
+    function getBudgetExpenses(budgetId) {
+        return expenses.filter(expense => expense.budgetId === budgetId)
+
+    }
+    function addExpense({ description, amount, budgetId }) {
+        setExpenses( prevExpenses => {
+            return [...prevExpenses, { id: uuidv4(), description, amount, budgetId }]
+        })
+
+    }
+    function addBudget({ name, max }) {
+        setBudgets(prevBudgets => {
+            if( prevBudgets.find(budget => budget.name === name) ) {
+                return prevBudgets
+            }
+            return [...prevBudgets, {id: uuidv4(), name, max}]
+        })
+
+    }
+    function deleteBudget( {id} ) {
+        setBudgets(prevBudgets => {
+            return prevBudgets.filter(budget => budget.id !== id)
+        })
+
+    }
+    function deleteExpense( {id} ) {
+        setExpenses(prevExpenses => {
+            return prevExpenses.filter(expense => expense.id !== id)
+        })
+
+    }
+
+    return (
+        <BudgetContext.Provider value={{
+            budgets,
+            expenses,
+            getBudgetExpenses,
+            addExpense,
+            addBudget,
+            deleteBudget,
+            deleteExpense
+        }}>{children}</BudgetContext.Provider>
+    )
+    
+}
