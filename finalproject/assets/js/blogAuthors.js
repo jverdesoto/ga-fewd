267af74213ada3160b592d33c70ddd89c4d16e90
Henry Pearce <hal_pearce@hotmail.co.uk>
2023-09-21 19:32:11 +0100

    finalproject 21/09/2023

--- finalproject/assets/js/blogAuthors.js
+++ finalproject/assets/js/blogAuthors.js
@@ -3,20 +3,24 @@ var myAuthors = [
         blogAuthorId: 1,
         blogAuthor: "John Smith",
         blogAuthorGender: "Male",
+        blogAuthorImage: "johnsmith.jpg"
     },
     {
         blogAuthorId: 2,
         blogAuthor: "Matthew Robertson",
         blogAuthorGender: "Male",
+        blogAuthorImage: "matthewrobertson.jpg"
     },
     {
         blogAuthorId: 3,
         blogAuthor: "Edward Smith",
         blogAuthorGender: "Male",
+        blogAuthorImage: "edwardsmith.jpg"
     },
     {
         blogAuthorId: 4,
         blogAuthor: "Jane Smith",
         blogAuthorGender: "Female",
+        blogAuthorImage: "janesmith.jpg"
     },
 ]
