d2c0cb37d17fbea31e35226ae058748f154b0b77
Areej Almarri <areej@Areejs-MacBook-Pro-3.local>
2023-09-27 23:17:35 +0300

    Detailspage

---
+++ final/DetailsPage.js
@@ -0,0 +1,68 @@
+async function loadMovieDetails() {
+    const response = await fetch(`https://ghibliapi.vercel.app/films/${new URLSearchParams(window.location.search).get('movie_id')}`);
+    const movieDetails = await response.json();
+    const moviePosterContainer = document.getElementById('poster');
+    const poster=document.createElement('img')
+    poster.src=movieDetails.image
+    poster.alt=`${movieDetails.title} Poster`
+    poster.style='max-width: 20%; height: auto; margin: 0 auto;'
+    moviePosterContainer.append(poster)
+    const movieDetailsContainer = document.getElementById('movie_details_container');
+    const element = document.createElement('div');
+    element.innerHTML = `
+            <div class="row" style="margin-bottom: 32px;">
+                <h2>${movieDetails.title}</h2>
+            </div>
+            <div class="row" style="margin-bottom: 24px;">
+                <div class="col-3"><b>Original Name:</b> ${movieDetails.original_title ?? null}</div>
+                <div class="col-3"><b>Director:</b> ${movieDetails.director ?? null}</div>
+                <div class="col-3"><b>Producer:</b> ${movieDetails.producer ?? null}</div>
+                <div class="col-3"><b>Rating Score:</b> ${movieDetails.rt_score ?? null}</div>
+            </div>
+            <div class="row" style="margin-bottom: 42px;">
+                <div class="col-3"><b>Release Date:</b> ${movieDetails.release_date ?? null}</div>
+                <div class="col-3"><b>Running Time:</b> ${movieDetails.running_time ?? null} min</div>
+            </div>
+            <div class="row" style="margin-bottom: 24px;">
+                <h4>About The Movie</h4>
+            </div>
+            <div class="row" style="margin-bottom: 42px;">
+                <p>${movieDetails.description ?? null}</p>
+            </div>
+            <div class="row" style="margin-bottom: 24px;">
+                <h4>Characters</h4>
+            </div>
+                    `;
+    if (movieDetails.people && movieDetails.people.length > 0 && movieDetails.people[0] != 'https://ghibliapi.vercel.app/people/') {
+        let characterCount = 0;
+        let characterRow = ""; 
+        const characters = await Promise.all(movieDetails.people.map(async characterUrl => {
+            const response = await fetch(characterUrl);
+            const characterDetails = await response.json();
+            return characterDetails;
+          }));
+        characters.forEach(character => {
+            characterRow += `
+                                <div class="col-3">${character.name}</div>
+                            `;
+
+            characterCount++; 
+
+            if (characterCount % 4 === 0 || characterCount === movieDetails.people.length) {
+                element.innerHTML += `
+                                    <div class="row" style="margin-bottom: 24px;">
+                                        ${characterRow}
+                                    </div>
+                                `;
+
+                characterRow = "";
+            }
+        });
+    }
+    else{
+        element.innerHTML += `<div class="row" style='margin-left:0px'>No Data</div>`;
+    }
+    movieDetailsContainer.appendChild(element);
+}
+
+window.onload = loadMovieDetails;
--- final/HomePage.js
+++ final/HomePage.js
@@ -3,21 +3,24 @@ async function loadMovies() {
     const movies = await response.json();
     const movieContainer = document.getElementById('movie_container');
     movies.forEach(movie => {
-        console.log(movie);
+        if(movie.description.length > 55){
+            movie.description = movie.description.substr(0,55) + "..."
+        }
         const card = document.createElement('div');
-        card.className = 'col-12 col-sm-6 col-md-4 col-lg-2';
+        card.className = 'col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2';
         card.innerHTML = `
-                        <div class="card">
+                        <div class="card" style='height :633px;'>
                             <img src="${movie.image}" class="card-img-top" alt="${movie.title} Poster">
                             <div class="card-body">
                                 <h5 class="card-title">${movie.title}</h5>
                                 <p class="card-text">${movie.original_title}</p>
                                 <p class="card-text">${movie.description}</p>
-                                <a href="#" class="btn btn-primary">Read More</a>
+                                <a href="DetailsPage.html?movie_id=${movie.id}" class="btn" style="background-color:#4DAAE9; color: #ffffff;">Read More</a>
                             </div>
                         </div>
                     `;
         movieContainer.appendChild(card);
     })
 }
-window.onload = loadMovies
+
+window.onload =loadMovies;
